6c224eed6a8b3f98bbc9561074370ff5a128d74e
mohammadalomer1991 <mohammadalomer1991@gmail.com>
2025-11-04 09:10:50 +0900

    successfull

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,19 +1,11 @@
 // lib/infra-stack.ts
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { NewVPC } from './create-vpc'; // Now this works!
 
 export class InfraStack extends cdk.Stack {
   public readonly vpc: cdk.aws_ec2.Vpc;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // Instantiate your reusable construct
-    const myVpc = new NewVPC(this, 'MyVpcConstruct', {
-      cidr: '10.0.0.0/16'
-    });
-    // Expose the vpc property from the construct
-    this.vpc = myVpc.vpc;
   }
 }
